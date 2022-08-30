b4151880f2ef5e43193a62f2b01b6f07d649c64e
matapatos <andre_gil22@hotmail.com>
2022-08-30 19:46:52 +0100

    Tests - Loading WP in all types of tests instead of only integration tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -13,43 +13,41 @@ require_once "constants.php";
  * add additional argument to the test run command if you want to run
  * integration tests.
  */
-if (isset($GLOBALS['argv']) && in_array('--group=integration', $GLOBALS['argv'], true)) {
-	if (!file_exists(dirname(__FILE__, 2) . '/wp/tests/phpunit/wp-tests-config.php')) {
-		// We need to set up core config details and test details
-		copy(
-			dirname(__FILE__, 2) . '/wp/wp-tests-config-sample.php',
-			dirname(__FILE__, 2) . '/wp/tests/phpunit/wp-tests-config.php'
-		);
+if (!file_exists(dirname(__FILE__, 2) . '/wp/tests/phpunit/wp-tests-config.php')) {
+	// We need to set up core config details and test details
+	copy(
+		dirname(__FILE__, 2) . '/wp/wp-tests-config-sample.php',
+		dirname(__FILE__, 2) . '/wp/tests/phpunit/wp-tests-config.php'
+	);
 
-		// Change certain constants from the test's config file.
-		$testConfigPath = dirname(__FILE__, 2) . '/wp/tests/phpunit/wp-tests-config.php';
-		$testConfigContents = file_get_contents($testConfigPath);
+	// Change certain constants from the test's config file.
+	$testConfigPath = dirname(__FILE__, 2) . '/wp/tests/phpunit/wp-tests-config.php';
+	$testConfigContents = file_get_contents($testConfigPath);
 
-		$testConfigContents = str_replace(
-			"dirname( __FILE__ ) . '/src/'",
-			"dirname(__FILE__, 3) . '/src/'",
-			$testConfigContents
-		);
-		$testConfigContents = str_replace("youremptytestdbnamehere", $_SERVER['DB_NAME'], $testConfigContents);
-		$testConfigContents = str_replace("yourusernamehere", $_SERVER['DB_USER'], $testConfigContents);
-		$testConfigContents = str_replace("yourpasswordhere", $_SERVER['DB_PASSWORD'], $testConfigContents);
-		$testConfigContents = str_replace("localhost", $_SERVER['DB_HOST'], $testConfigContents);
+	$testConfigContents = str_replace(
+		"dirname( __FILE__ ) . '/src/'",
+		"dirname(__FILE__, 3) . '/src/'",
+		$testConfigContents
+	);
+	$testConfigContents = str_replace("youremptytestdbnamehere", $_SERVER['DB_NAME'], $testConfigContents);
+	$testConfigContents = str_replace("yourusernamehere", $_SERVER['DB_USER'], $testConfigContents);
+	$testConfigContents = str_replace("yourpasswordhere", $_SERVER['DB_PASSWORD'], $testConfigContents);
+	$testConfigContents = str_replace("localhost", $_SERVER['DB_HOST'], $testConfigContents);
 
-		file_put_contents($testConfigPath, $testConfigContents);
-	}
-
-	// Give access to tests_add_filter() function.
-	require_once dirname(__FILE__, 2) . '/wp/tests/phpunit/includes/functions.php';
-
-	/**
-	 * Manually load the plugin being tested.
-	 */
-	function _manually_load_plugin()
-	{
-		require dirname(dirname(__FILE__)) . '/wp-fastendpoints.php';
-	}
-
-	tests_add_filter('muplugins_loaded', '_manually_load_plugin');
-
-	require_once dirname(__FILE__, 2) . '/wp/tests/phpunit/includes/bootstrap.php';
+	file_put_contents($testConfigPath, $testConfigContents);
 }
+
+// Give access to tests_add_filter() function.
+require_once dirname(__FILE__, 2) . '/wp/tests/phpunit/includes/functions.php';
+
+/**
+ * Manually load the plugin being tested.
+ */
+function _manually_load_plugin()
+{
+	require dirname(dirname(__FILE__)) . '/wp-fastendpoints.php';
+}
+
+tests_add_filter('muplugins_loaded', '_manually_load_plugin');
+
+require_once dirname(__FILE__, 2) . '/wp/tests/phpunit/includes/bootstrap.php';
