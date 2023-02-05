3b8f40b4d530f1edb82dd4dcaa19173160104160
matapatos <andre_gil22@hotmail.com>
2023-02-05 16:17:32 +0000

    Tests - Including composer autoloader

--- tests/Pest.php
+++ tests/Pest.php
@@ -2,6 +2,9 @@
 
 use Yoast\WPTestUtils\BrainMonkey\TestCase;
 
+$composer = __DIR__ . '/../vendor/autoload.php';
+require_once $composer;
+
 uses()->group('integration')->in('Integration');
 uses()->group('unit')->in('Unit');
 
