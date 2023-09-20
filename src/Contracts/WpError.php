919e8af21e377432e0f2c919184e94972dd09e1b
matapatos <andre_gil22@hotmail.com>
2023-09-20 18:25:09 +0100

    Fixed issue of not returning correct error message

--- src/Contracts/WpError.php
+++ src/Contracts/WpError.php
@@ -18,11 +18,38 @@ class WpError extends WP_Error
 {
 	/**
 	 * @since 0.9.0
-	 * @param string $message - The error message.
+	 * @param string|array $message - The error message.
 	 */
-	public function __construct(int $statusCode, string $message, array $data = [])
+	public function __construct(int $statusCode, $message, array $data = [])
 	{
+		if (is_array($message)) {
+			$data['all_messages'] = $message;
+		}
+		$message = $this->getFirstErrorMessage($message);
 		$data = array_merge(['status' => $statusCode], $data);
 		parent::__construct($statusCode, esc_html__($message), $data);
+	}
+
+	/**
+	 * Gets the first message from an array
+	 *
+	 * @param string|array $message
+	 * @return string
+	 */
+	protected function getFirstErrorMessage($message): string
+	{
+		if (!is_array($message)) {
+			return $message;
+		}
+
+		if (count($message) == 0) {
+			return 'No error description provided';
+		}
+
+		$data['all_messages'] = $message;
+		while (is_array($message)) {
+			$message = reset($message);
+		}
+		return $message;
 	}
 }
